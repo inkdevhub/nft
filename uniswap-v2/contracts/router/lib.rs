edf0c4aa5b1235c6f17140179009b19c174f2df4
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2022-09-09 01:17:53 +0900

    up

--- uniswap-v2/contracts/router/lib.rs
+++ uniswap-v2/contracts/router/lib.rs
@@ -4,12 +4,48 @@
 mod router {
     use ink_storage::traits::SpreadAllocate;
     use ink_prelude::vec::Vec;
+    use ink_env::CallFlags;
     use openbrush::traits::{
         ZERO_ADDRESS
     };
+    use openbrush::contracts::{
+        psp22::PSP22Error,
+        traits::psp22::PSP22Ref,
+    };
 
-    use uniswap_v2::traits::pair::PairRef;
+    use uniswap_v2::traits::pair::{PairRef, PairError};
+    use uniswap_v2::traits::factory::{FactoryRef, FactoryError};
 
+    // Error Definition
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum RouterError {
+        PSP22Error(PSP22Error),
+        FactoryError(FactoryError),
+        PairError(PairError),
+        Other,
+    }
+
+    impl From<PSP22Error> for RouterError {
+        fn from(error: PSP22Error) -> Self {
+            RouterError::PSP22Error(error)
+        }
+    }
+
+    impl From<FactoryError> for RouterError {
+        fn from(error: FactoryError) -> Self {
+            RouterError::FactoryError(error)
+        }
+    }
+
+    impl From<PairError> for RouterError {
+        fn from(error: PairError) -> Self {
+            RouterError::PairError(error)
+        }
+    }
+
+    // Returned Value Definition
+    
     #[ink(storage)]
     #[derive(Default, SpreadAllocate)]
     pub struct Router {
@@ -57,8 +93,22 @@ mod router {
         }
 
         #[ink(message)]
-        pub fn add_liquidity(&mut self, token_a: AccountId, token_b: AccountId, amount_a_desired: Balance, amount_b_desired: Balance, amount_a_min: Balance, amount_b_min: Balance) -> (Balance, Balance, Balance) {
+        pub fn add_liquidity(
+            &mut self,
+            token_a: AccountId,
+            token_b: AccountId,
+            amount_a_desired: Balance,
+            amount_b_desired: Balance,
+            amount_a_min: Balance,
+            amount_b_min: Balance
+        ) -> Result<(Balance, Balance, Balance), RouterError> {
+            let (amount_a, amount_b) = self._add_liquidity(token_a, token_b, amount_a_desired, amount_b_desired, amount_a_min, amount_b_min)?;
+            let pair_contract = self._pair_for(self.factory, token_a, token_b);
+            self._safe_transfer(token_a, pair_contract, amount_a)?;
+            self._safe_transfer(token_b, pair_contract, amount_b)?;
+            let liquidity = PairRef::mint(&pair_contract, self.env().caller())?;
             
+            Ok((amount_a, amount_b, liquidity))
         }
 
         fn _quote(&self, amount_a: Balance, reserve_a: Balance, reserve_b: Balance) -> Balance {
@@ -103,6 +153,61 @@ mod router {
             let (token_0, token_1) = if token_a < token_b { (token_a, token_b) } else { (token_b, token_a) };
             assert!(token_0 != ZERO_ADDRESS.into(), "ZERO ADDRESS");
             (token_0, token_1)
+        }
+
+        fn _add_liquidity(
+            &mut self,
+            token_a: AccountId,
+            token_b: AccountId,
+            amount_a_desired: Balance,
+            amount_b_desired: Balance,
+            amount_a_min: Balance,
+            amount_b_min: Balance,
+        ) -> Result<(Balance, Balance), RouterError> {
+            if FactoryRef::get_pair(&self.factory, token_a, token_b).is_none() {
+                FactoryRef::create_pair(&self.factory, token_a, token_b)?;
+            };
+            
+            let (reserve_a, reserve_b) = self._get_reserves(self.factory, token_a, token_b);
+
+            if reserve_a == 0 && reserve_b == 0 {
+                return Ok((amount_a_desired, amount_b_desired))
+            }
+
+            let amount_b_optimal = self._quote(amount_a_desired, reserve_a, reserve_b);
+            if amount_b_optimal <= amount_b_desired {
+                assert!(amount_b_min <= amount_b_optimal, "INSUFFICIENT B AMOUNT");
+                Ok((amount_a_desired, amount_b_optimal))
+            } else {
+                let amount_a_optimal = self._quote(amount_b_desired, reserve_b, reserve_a);
+                assert!(amount_a_optimal <= amount_a_desired);
+                assert!(amount_a_min <= amount_a_optimal, "INSUFFICIENT A AMOUNT");
+                Ok((amount_a_optimal, amount_b_desired))
+            }
+        }
+
+        fn _pair_for(&self, factory: AccountId, token_a: AccountId, token_b: AccountId) -> AccountId {
+            let (token_0, token_1) = self._sort_tokens(token_a, token_b);
+
+            // Original Uniswap Library pairFor function calculate pair contract address without making external calls.
+            // Please refer https://github.com/Uniswap/v2-periphery/blob/master/contracts/libraries/UniswapV2Library.sol#L18
+            
+            // In this contract, use external call to get pair contract address.
+            let pair = FactoryRef::get_pair(&self.factory, token_0, token_1).unwrap();
+            pair
+        }
+
+        fn _safe_transfer(
+            &mut self,
+            token: AccountId,
+            to: AccountId,
+            value: Balance,
+        ) -> Result<(), RouterError> {
+            PSP22Ref::transfer_builder(&token, to, value, Vec::<u8>::new())
+                .call_flags(CallFlags::default().set_allow_reentry(true))
+                .fire()
+                .unwrap()?;
+            Ok(())
         }
     }
 
