23a126ed657168ba3c69015f6f171c35ad367dea
Pierre Giraud <35110271+PierreOssun@users.noreply.github.com>
2022-11-04 10:55:07 +0100

    Farming contract (#16)
    * Added farming contract
    
    * Added storage + events
    
    * separated Traits
    
    * Added Ownable + add
    
    * update pool + storage refacto
    
    * Provide tentative stake/unstake features
    
    * Implement add correctly
    
    * Fix deposit (initialization is needed)
    
    * Refactor rewarders structure
    
    * updated toolchain
    
    * Finished update all pools
    
    * pending arsw
    
    * set function
    
    * fix mut of var
    
    * fmt
    
    * clean code - pr comments
    
    * refactor file structure
    
    * Impl rewarder
    
    * withdraw & harvest
    
    * Finished update all pools
    
    * pending arsw
    
    * set function
    
    * clean code - pr comments
    
    * refactor file structure
    
    * Impl rewarder
    
    * withdraw & harvest
    
    * Pr comments
    
    * Pr comments
    
    * Pr comments
    
    * Pr comments
    
    * Added deposit_arsw + emergency_withdraw + withdraw_and_harvest
    
    * refactor guards
    
    * PR comments - clean code
    
    * PR comments
    
    * cast as u128
    
    * Fixed error enum
    
    Co-authored-by: Hyunggyu Jang <murasakipurplez5@gmail.com>

--- farming/logics/traits/master_chef/getters.rs
+++ farming/logics/traits/master_chef/getters.rs
@@ -1,5 +1,8 @@
-use crate::traits::{
-    data::Pool,
+use crate::traits::master_chef::{
+    data::{
+        Pool,
+        UserInfo,
+    },
     farming::Data,
 };
 use ink_env::AccountId;
@@ -13,17 +16,27 @@ pub trait FarmingGetters: Storage<Data> {
     }
 
     #[ink(message)]
-    fn get_pool_infos(&self, pool_id: u32) -> Option<Pool> {
+    fn get_pool_info(&self, pool_id: u32) -> Option<Pool> {
         self.data::<Data>().pool_info.get(&pool_id)
     }
 
     #[ink(message)]
-    fn get_user_info(&self, pool_id: u32, user: AccountId) -> Option<(u128, i128)> {
+    fn get_user_info(&self, pool_id: u32, user: AccountId) -> Option<UserInfo> {
         self.data::<Data>().user_info.get(&(pool_id, user))
     }
 
     #[ink(message)]
     fn get_lp_token(&self, pool_id: u32) -> Option<AccountId> {
         self.data::<Data>().lp_tokens.get(pool_id as usize).copied()
+    }
+
+    #[ink(message)]
+    fn get_rewarder(&self, pool_id: u32) -> Option<AccountId> {
+        self.data::<Data>().rewarders.get(pool_id)
+    }
+
+    #[ink(message)]
+    fn get_total_alloc_point(&self) -> u32 {
+        self.data::<Data>().total_alloc_point
     }
 }
---
+++ farming/logics/traits/rewarder/rewarder.rs
@@ -0,0 +1,76 @@
+pub use crate::traits::rewarder::{
+    data::Data,
+    errors::RewarderError,
+    getters::RewarderGetters,
+};
+use ink_env::AccountId;
+use ink_prelude::vec::Vec;
+use openbrush::{
+    contracts::traits::psp22::PSP22Ref,
+    modifier_definition,
+    modifiers,
+    traits::{
+        Balance,
+        Storage,
+    },
+};
+
+// Cannot be 0 or it will panic!
+pub const REWARD_TOKEN_DIVISOR: u128 = 1_000_000_000_000_000_000;
+
+#[openbrush::wrapper]
+pub type RewarderRef = dyn Rewarder;
+
+#[openbrush::trait_definition]
+pub trait Rewarder: Storage<Data> + RewarderGetters {
+    #[ink(message)]
+    #[modifiers(only_master_chef)]
+    fn on_arsw_reward(
+        &self,
+        _pool_id: u32,
+        _user: AccountId,
+        to: AccountId,
+        arsw_amount: Balance,
+        _new_lp_amount: Balance,
+    ) -> Result<(), RewarderError> {
+        let pending_reward = arsw_amount
+            .checked_mul(self.reward_multiplier().into())
+            .ok_or(RewarderError::MulOverflow1)?
+            / REWARD_TOKEN_DIVISOR;
+        let reward_token = self.data::<Data>().reward_token;
+        let reward_bal = PSP22Ref::balance_of(&reward_token, Self::env().account_id());
+        if pending_reward < reward_bal {
+            PSP22Ref::transfer(&reward_token, to, reward_bal, Vec::new())?;
+        } else {
+            PSP22Ref::transfer(&reward_token, to, pending_reward, Vec::new())?;
+        }
+        Ok(())
+    }
+
+    #[ink(message)]
+    fn pending_tokens(
+        &self,
+        _pool_id: u32,
+        _user: AccountId,
+        arsw_amount: Balance,
+    ) -> Result<(AccountId, Balance), RewarderError> {
+        let reward_amount = arsw_amount
+            .checked_mul(self.reward_multiplier().into())
+            .ok_or(RewarderError::MulOverflow2)?
+            / REWARD_TOKEN_DIVISOR;
+        Ok((self.reward_token(), reward_amount))
+    }
+}
+
+#[modifier_definition]
+pub fn only_master_chef<T, F, R, E>(instance: &T, body: F) -> Result<R, E>
+where
+    T: Storage<Data>,
+    F: FnOnce(&T) -> Result<R, E>,
+    E: From<RewarderError>,
+{
+    if instance.data().master_chef != T::env().caller() {
+        return Err(From::from(RewarderError::CallerIsNotMasterChef))
+    }
+    body(instance)
+}
