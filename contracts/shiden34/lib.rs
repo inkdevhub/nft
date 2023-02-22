809966cb9648d8bb86a9d674f729513ddc48ca08
Mar.io <34627453+Maar-io@users.noreply.github.com>
2023-02-22 14:39:39 +0100

    ink4 updates

--- contracts/shiden34/lib.rs
+++ contracts/shiden34/lib.rs
@@ -1,35 +1,33 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![feature(min_specialization)]
-        
+
 #[openbrush::contract]
 pub mod shiden34 {
-    // imports from ink!
-	use ink_storage::traits::SpreadAllocate;
-
-    // imports from openbrush
-	use openbrush::traits::String;
-	use openbrush::traits::Storage;
-	use openbrush::contracts::ownable::*;
-	use openbrush::contracts::psp34::extensions::mintable::*;
-	use openbrush::contracts::psp34::extensions::enumerable::*;
-	use openbrush::contracts::psp34::extensions::metadata::*;
+    use openbrush::{
+        contracts::psp34::extensions::{
+            enumerable::*,
+            mintable::*,
+            metadata::*,
+        },
+        contracts::ownable::*,
+        traits::{Storage, String},
+    };
 
     #[ink(storage)]
-    #[derive(Default, SpreadAllocate, Storage)]
-    pub struct Contract {
-    	#[storage_field]
-		psp34: psp34::Data<Balances>,
+    #[derive(Default, Storage)]
+    pub struct Shiden34 {
+        #[storage_field]
+        psp34: psp34::Data<enumerable::Balances>,
 		#[storage_field]
 		ownable: ownable::Data,
-		#[storage_field]
+        #[storage_field]
 		metadata: metadata::Data,
     }
-    
-    // Section contains default implementation without any modifications
-	impl PSP34 for Contract {}
-	impl Ownable for Contract {}
-	impl PSP34Mintable for Contract {
-		#[ink(message)]
+
+    impl PSP34 for Shiden34 {}
+    impl Ownable for Shiden34 {}
+    impl PSP34Mintable for Shiden34 {
+        #[ink(message)]
 		#[openbrush::modifiers(only_owner)]
 		fn mint(
             &mut self,
@@ -38,20 +36,20 @@ pub mod shiden34 {
         ) -> Result<(), PSP34Error> {
 			self._mint_to(account, id)
 		}
-	}
-	impl PSP34Enumerable for Contract {}
-	impl PSP34Metadata for Contract {}
-     
-    impl Contract {
+    }
+    impl PSP34Enumerable for Shiden34 {}
+	impl PSP34Metadata for Shiden34 {}
+
+    impl Shiden34 {
         #[ink(constructor)]
         pub fn new() -> Self {
-            ink_lang::codegen::initialize_contract(|_instance: &mut Contract|{
-				_instance._init_with_owner(_instance.env().caller());
-				_instance._mint_to(_instance.env().caller(), Id::U8(1)).expect("Can't mint");
-				let collection_id = _instance.collection_id();
-				_instance._set_attribute(collection_id.clone(), String::from("name"), String::from("Shiden34"));
-				_instance._set_attribute(collection_id, String::from("symbol"), String::from("SH34"));
-			})
+            let mut instance = Self::default();
+            instance._init_with_owner(instance.env().caller());
+            instance._mint_to(instance.env().caller(), Id::U8(1)).expect("Can't mint");
+            let collection_id = instance.collection_id();
+            instance._set_attribute(collection_id.clone(), String::from("name"), String::from("Shiden34"));
+			instance._set_attribute(collection_id, String::from("symbol"), String::from("SH34"));
+            instance
         }
     }
 }
