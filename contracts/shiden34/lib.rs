f9f48a225c864bb0a548a48a3604ceae46c20ec1
Bobo <bobo.kovacevic@gmail.com>
2023-07-28 13:21:57 +0200

    Uplift to the latest ink! and Openbrush

--- contracts/shiden34/lib.rs
+++ contracts/shiden34/lib.rs
@@ -1,45 +1,53 @@
-#![cfg_attr(not(feature = "std"), no_std)]
-#![feature(min_specialization)]
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
 
+#[openbrush::implementation(PSP34, PSP34Enumerable, PSP34Metadata, PSP34Mintable, Ownable)]
 #[openbrush::contract]
 pub mod shiden34 {
-	use payable_mint_pkg::traits::payable_mint::*;
-    use openbrush::{
-        contracts::ownable::*,
-        contracts::psp34::extensions::{enumerable::*, metadata::*},
-        traits::{Storage, String},
-    };
+    use openbrush::traits::Storage;
+    use payable_mint_pkg::impls::payable_mint::*;
 
     #[ink(storage)]
     #[derive(Default, Storage)]
     pub struct Shiden34 {
         #[storage_field]
-        psp34: psp34::Data<enumerable::Balances>,
+        psp34: psp34::Data,
         #[storage_field]
         ownable: ownable::Data,
         #[storage_field]
         metadata: metadata::Data,
+        #[storage_field]
+        enumerable: enumerable::Data,
     }
 
-    impl PSP34 for Shiden34 {}
-    impl Ownable for Shiden34 {}
-    impl PSP34Enumerable for Shiden34 {}
-    impl PSP34Metadata for Shiden34 {}
-	impl PayableMint for Shiden34 {}
+    #[overrider(PSP34Mintable)]
+    #[openbrush::modifiers(only_owner)]
+    fn mint(&mut self, account: AccountId, id: Id) -> Result<(), PSP34Error> {
+        psp34::InternalImpl::_mint_to(self, account, id)
+    }
+
+    impl payable_mint::PayableMintImpl for Shiden34 {}
 
     impl Shiden34 {
         #[ink(constructor)]
         pub fn new() -> Self {
-            let mut instance = Self::default();
-            instance._init_with_owner(instance.env().caller());
-            let collection_id = instance.collection_id();
-            instance._set_attribute(
+            let mut _instance = Self::default();
+            ownable::Internal::_init_with_owner(&mut _instance, Self::env().caller());
+            psp34::Internal::_mint_to(&mut _instance, Self::env().caller(), Id::U8(1))
+                .expect("Can mint");
+            let collection_id = psp34::PSP34Impl::collection_id(&_instance);
+            metadata::Internal::_set_attribute(
+                &mut _instance,
                 collection_id.clone(),
                 String::from("name"),
                 String::from("Shiden34"),
             );
-            instance._set_attribute(collection_id, String::from("symbol"), String::from("SH34"));
-            instance
+            metadata::Internal::_set_attribute(
+                &mut _instance,
+                collection_id,
+                String::from("symbol"),
+                String::from("SH34"),
+            );
+            _instance
         }
     }
 }
